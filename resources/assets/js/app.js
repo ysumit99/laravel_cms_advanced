eb2a53dba60411ab97a60e1199347e2e29d0120c
sumit23 <ysumit99@gmail.com>
2017-07-25 04:31:23 +0530

    Initial commit!!

---
+++ resources/assets/js/app.js
@@ -0,0 +1,14 @@
+
+
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
+
+
+
+//Vue.component('example', require('./components/Example.vue'));
+
